5fc09fecce63602156931ff0aae2e5f6140242a3
suryaprakash539 <surya152p1a0539@gmail.com>
2019-11-06 10:02:37 +0530

    made changes to index.js file

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
-
+import axios from './config/axios'
+import {setUser} from './actions/userAction'
+import {setContact} from './actions/contactAction'
 import {Provider} from 'react-redux'
 import configureStore from './store/configureStore'
 
@@ -12,29 +13,29 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 const store=configureStore()
 //console.log(store.getState())
 
-store.subscribe(()=>{
-    console.log(store.getState())
-})
+// store.subscribe(()=>{
+//     console.log(store.getState())
+// })
 
-//handle page reloads
-// if(localStorage.getItem('userAuthToken')){
-//     axios.get('/users/account',{headers:{
-//         'x-auth':localStorage.getItem('userAuthToken')
-//     }})
-//     .then(response=>{
-//         store.dispatch(setUser(response.data))
-//     })
+if(localStorage.getItem('userAuthToken')){
+ //handle page reloads
+  axios.get('/users/account',{headers:{
+        'x-auth':localStorage.getItem('userAuthToken')
+    }})
+    .then(response=>{
+        store.dispatch(setUser(response.data))
+    })
 
-//     axios.get(`/contacts`,{
-//         headers: {
-//             'x-auth': localStorage.getItem('userAuthToken')
-//         }
-//     })
-//         .then(response=>{
+    axios.get(`/contacts`,{
+        headers: {
+            'x-auth': localStorage.getItem('userAuthToken')
+        }
+    })
+        .then(response=>{
             
-//             store.dispatch(setContact(response.data))
-//         })
-// }
+            store.dispatch(setContact(response.data))
+        })
+}
 
 const ele=(
     <Provider store={store}>
